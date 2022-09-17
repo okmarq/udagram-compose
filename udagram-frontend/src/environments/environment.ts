135d96f123579004d00be17884d05dfdfcf09875
Joel Okoromi <okmarq@gmail.com>
2022-09-17 16:33:21 +0100

    environment updated for frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a7ac4d99b8d93431aba3bf2a179b14ae-452380603.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
